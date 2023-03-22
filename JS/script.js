003af3e044ff4ab1dd0c96168412bcf883fbca90
Mitchell Nelson <mitchell@devpipeline.com>
2023-03-22 17:37:19 -0600

    weight buttons

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,44 @@
-fetch("MOCK_DATA.json", {})
+async function grabData() {
+  try{
+    const responseData = await fetch("MOCK_DATA.json", {})
+    .then( res => res.json())
+    return responseData
+  } catch (error) {
+    console.error(error);
+  }
+}
+console.log(grabData())
 
-.then(res => res.json())
+function capitalizeName(firstName, lastName) {
+  return firstName[0].toUppeerCase() + firstName.
+  slice(1)+ ` ` + lastName[0].toUppeerCase()
 
-.then(data => console.log(data))
+} 
 
-.catch(err => console.log(err))
+function addToHTML(fullName) {
+  const students = document.getElementsByClassName
+  ("bottom-weight-wrapper")[0]
+  const div = document.createElement("div")
+  const add = document.createTextNode("+")
+  const minus = document.createTextNode("-")
+  const buttonDiv = document.createElement("div")
+  const paragraph = document.createElement("p")
+  const addButton = document.createElement("button")
+  const minusButton = document.createElement("button")
 
+    div.appendChild(paragraph)
+    paragraph.appendChild(fullName)
+    div.classList.add("all-students")
+    console.log(students)
+    students.appendChild(div)
+    
+      div.appendChild(buttonDiv)
+      buttonDiv.appendChild(addButton)
+      buttonDiv.appendChild(minusButton)
+      addButton.appendChild(add)
+      minusButton.appendChild(minus)
+      students.appendChild(div)
 
+}
+  
+addToHTML(document.createTextNode("happy"))
