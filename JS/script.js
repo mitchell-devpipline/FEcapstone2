20f0fee3fb6cb1292b2356f7f31a66c733e22ff9
Mitchell Nelson <mitchell@devpipeline.com>
2023-03-24 08:14:07 -0600

    style-fixes

--- JS/script.js
+++ JS/script.js
@@ -12,43 +12,48 @@ function capitalizeName(firstName, lastName) {
   return firstName[0].toUpperCase() + firstName.slice(1)+ ` ` + lastName[0].toUpperCase()
 }
 
-function addToHTML(fullName) {
+function addToHTML(fullName, weight) {
   const students = document.getElementsByClassName
   ("bottom-weight-wrapper")[0]
-  const div = document.createElement("div")
   const add = document.createTextNode("+")
   const minus = document.createTextNode("-")
   const buttonDiv = document.createElement("div")
+  const div = document.createElement("div")
+  const weightNum = document.createTextNode(weight)
   const paragraph = document.createElement("p")
   const addButton = document.createElement("button")
   const minusButton = document.createElement("button")
 
     div.appendChild(paragraph)
     paragraph.appendChild(fullName)
-    div.classList.add("all-students")
+    paragraph.append(weightNum)
     console.log(students)
+    div.classList.add("users")
     students.appendChild(div)
     
       div.appendChild(buttonDiv)
       buttonDiv.appendChild(addButton)
       buttonDiv.appendChild(minusButton)
       addButton.appendChild(add)
+      addButton.classList.add("click-add")
       minusButton.appendChild(minus)
       students.appendChild(div)
-
+      
+      // addButton.addEventListener("onclick", )s
 }
 // addToHTML(document.createTextNode(fullName))
   
 
-async function split() {
+async function weightControl() {
   const data = await grabData()
 
   data.forEach(students => {
-    const names = document.createTextNode(`${capitalizeName(students.first_name, students.last_name)}`)
+    const names = document.createTextNode(`${capitalizeName(students.first_name, students.last_name)} : `)
     console.log(names)
-    addToHTML(names)
-    // console.log(students)
+    const updateWeight = (students["userWeightControl"] = 1)
+    addToHTML(names, updateWeight)
   })
 }
 
-split()
+weightControl()
+
