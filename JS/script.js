407c9948d4b6f434de8257859d75dcdb6bde536f
mitchell-devpipline <122054442+mitchell-devpipline@users.noreply.github.com>
2023-03-27 14:57:15 -0600

    add-minus-weight

--- JS/script.js
+++ JS/script.js
@@ -39,110 +39,41 @@ function addToHTML(fullName, weightCounter) {
   addButton.appendChild(add)
   addButton.classList.add("click-add")
   minusButton.appendChild(minus)
-  students.appendChild(div)
-      
-      // addButton.addEventListener("onclick", )s
+  minusButton.classList.add("click-minus")
+  students.appendChild(div);
+
+  addButton.addEventListener("click" , e => {
+    handleWeight(e, 1)
+  })
+  
+  minusButton.addEventListener("click" , e => {
+    handleWeight(e, -1)
+  })
+  
 }
-// addToHTML(document.createTextNode(fullName))
-  
 
-// async function weightControl() {
-//   const data = await grabData()
-//   let weightCounter = 1;
-  
-//   data.forEach(students => {
-//     const names = document.createTextNode(`${capitalizeName(students.first_name, students.last_name)} : `)
-//     // console.log(names)
-//     const updateWeight = (students["userWeightControl"] = weightCounter)
-//     addToHTML(names, updateWeight)
-//     // document.getElementsByClassName(".addButton").addEventListener("click", () => {
-//     // count++})
-//   })
-// }
+function handleWeight(e, amt) {
+  const weightEl = e.target.parentElement.previousElementSibling
 
-// weightControl()
-
+  weightEl.innerText = Number(weightEl.innerText) + amt
+}
 
 
 
 async function weightControl(weightCounter) {
   const data = await grabData()
-  // console.log(weightCounter)
   
   data.forEach(students => {
     const names = document.createTextNode(`${capitalizeName(students.first_name, students.last_name)} : `)
     const updateWeight = weightCounter
 
     addToHTML(names, updateWeight)
-    add(weightCounter)
   })
 }
+
+weightControl(weightCounter)
+
 
 // nameChanger = document.querySelector("p").innerHTML = "hi"
 // This will update the spot for adding in a name
 
-
-function add(weightCounter) {
-  document.querySelector(".click-add").addEventListener("click", () => {
-  
-  var elems = document.body.getElementsByTagName("*");
-  console.log(elems)
-  
-  numberCount = document.querySelector(".users h1").innerHTML = weightCounter++
-  console.log(numberCount)
-  })
-  
-  }
-
-
-
-
-
-// function add(weightCounter) {
-// document.querySelector(".click-add").addEventListener("click", () => {
-
-// var elems = document.body.getElementsByTagName("*");
-// console.log(elems)
-
-// button = document.querySelector(".click-add")
-// button.setAttribute("button", weightCounter++);
-// console.log(weightCounter)})
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-<body>
-
-<h2>JavaScript addEventListener()</h2>
-
-<p>This example uses the addEventListener() method to attach a click event to a button.</p>
-
-<button id="myBtn">Try it</button>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("myBtn").addEventListener("click", displayDate);
-
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-}
-</script>
-
-</body>
-*/
-
-weightControl(weightCounter)
